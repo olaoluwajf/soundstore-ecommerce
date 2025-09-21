f257a5edb5e812b1d1b973700efca88458480a02
Joshua Famiroju <famirojujoshua@gmail.com>
2025-09-21 19:21:03 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Hero from './Components/Hero';
 import ProductPage from './Components/ProductPage';
