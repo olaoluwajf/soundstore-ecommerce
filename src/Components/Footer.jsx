fc7927edf60f454e043bb1d2a456766c0c3d49c1
Joshua Famiroju <famirojujoshua@gmail.com>
2025-09-24 12:57:40 +0100

    responsitivity issues fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     return(
         <div className='footer-section'>
 <Link to='/' className="footer-logo">SoundStore</Link>
-<div classname='rights'>
+<div className='rights'>
     <p>All Rights Reserved - ©2025</p>
 </div>
-<div classname='credits'><p>Built by <a href='https://github.com/olaoluwajf' target='_blank'>
+<div className='credits'><p>Built by <a href='https://github.com/olaoluwajf' target='_blank'>
 Joshua Famiroju</a></p>
 </div>
         </div>
